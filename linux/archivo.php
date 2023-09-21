950950c354989e4a6864e14cf0aa97ebad8ecb7c
MDLR <mec.13@hotmail.com>
2023-09-21 09:00:07 -0500

    mejorando linux por partes

--- linux/archivo.php
+++ linux/archivo.php
@@ -1,12 +1,37 @@
 <?php include('../includes/header.php'); ?>
 <?php
+if (isset($_GET['parte'])) {
+    $parte = $_GET['parte'];
+    if ($parte == 0) {
+        echo 'Archivos ls                         listar archivos sin lista
+ls -a                      listar todo archivo sin lista
+ls -al                     listar todo archivo (normales + ocultos) en lista';
+    } elseif ($parte == 1) {
+        echo 'touch archivo.extension    crear archivo vacio
+        rm archivo.extension       remove eliminar archivo
+        cp                         copiar un archivo a otro
+        mv                         mover archivos de una ubicacion a otra';
+    } elseif ($parte == 2) {
+        echo 'abrir archivos
 
-// Parte 1
-echo 'Esta es la parte 1';
-// parte 2
-include('archivo.php?parte=2');
-// Parte 3
-echo 'Esta es la parte 3';
+        code nombrr.txt            abrir codigo en vsc
+        cat archivo.extension      muestra archivo en consola';
+    } elseif ($parte == 3) {
+      echo 'Enlaces
+
+      pwd                               Enlace actual de ubicacion
+      cd/                               cambiar directorio (volver al home)
+      ~                                 variable con la url del home
+      cd/mnt/letradiscoduro             cambiar disco duro
+      cd .                              carpeta actual
+      cd ..                             regresar una carpeta';
+    }
+    else {
+        echo 'Parte no válida';
+    }
+} else {
+    echo 'Por favor, selecciona una parte.';
+}
 
 ?>
 <?php include('../inform.php'); ?>
